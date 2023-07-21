7cd44076748d0e765c5f382a94a396a04e912c72
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-07-21 16:56:48 +0200

    Fix an error

--- src/constants.py
+++ src/constants.py
@@ -7,13 +7,13 @@ AVAILABLE_LLMS = [
         "rowtype": "action",
         "title": _("BAI Chat"),
         "description": _("BAI Chat is a GPT-3.5 / ChatGPT API based chatbot that is free, convenient and responsive."),
-        "class": GPT4AllHandler
+        "class": BaiHandler
     },
     {
         "key": "local",
         "rowtype": "expander",
         "title": _("Local Model"),
         "description": _("Run a LLM model locally, more privacy but slower"),
-        "class": BaiHandler
+        "class": GPT4AllHandler
     }
 ]
