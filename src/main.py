40af16030b0526887c86ba61a203d9cfd9762f7b
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-08-16 16:39:10 +0200

    Initial Live2D idea

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 import gi, os
 
 gi.require_version('Gtk', '4.0')
+gi.require_version('WebKit', '6.0')
 gi.require_version('GtkSource', '5')
 gi.require_version('Adw', '1')
 import pickle
