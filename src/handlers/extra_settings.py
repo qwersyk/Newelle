3f87d1198f4df91ce3a68e4f856cd5e386543717
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-03-13 21:02:16 +0100

    Add documentation for extra settings

--- src/handlers/extra_settings.py
+++ src/handlers/extra_settings.py
@@ -5,6 +5,23 @@ from collections.abc import Callable
 class ExtraSettings:
     @staticmethod 
     def Setting(key: str, title: str, description: str, default: object, folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new setting
+
+        Args:
+            key: key of the setting, used to be retrived 
+            title: title of the setting, shown to the user 
+            description: description of the setting, shown to the user 
+            default: default value of the setting 
+            folder: if not None, near the setting it will be shown a button to open the specified folder 
+            website: if not None, near the setting it will be shown a button to open the specified website 
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed 
+            refresh_icon: if not None, the icon of the refresh button 
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = {
             "key": key,
             "title": title,
@@ -25,6 +42,23 @@ class ExtraSettings:
     @staticmethod
     def EntrySetting(key:str, title: str, description: str, default: str, 
                      folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new entry setting, which can be used to enter a string
+
+        Args:
+            key: key of the setting 
+            title: title of the setting  
+            description: description of the setting 
+            default: default value of the setting 
+            folder: if not None, near the setting it will be shown a button to open the specified folder 
+            website: if not None, near the setting it will be shown a button to open the specified website 
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed 
+            refresh_icon: if not None, the icon of the refresh button 
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = ExtraSettings.Setting(key, title, description, default, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "entry"
         return r
@@ -32,6 +66,23 @@ class ExtraSettings:
     @staticmethod
     def MultilineEntrySetting(key:str, title: str, description: str, default: str, 
                      folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new entry setting, which can be used to enter a string with multiple line (shown as an expander row)
+
+        Args:
+            key: key of the setting 
+            title: title of the setting 
+            description: description of the setting 
+            default: default value of the setting 
+            folder: if not None, near the setting it will be shown a button to open the specified folder 
+            website: if not None, near the setting it will be shown a button to open the specified website 
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed 
+            refresh_icon: if not None, the icon of the refresh button 
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = ExtraSettings.Setting(key, title, description, default, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "multilineentry"
         return r
@@ -39,6 +90,23 @@ class ExtraSettings:
     @staticmethod
     def ToggleSetting(key:str, title: str, description: str, default: bool, 
                       folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new toggle setting. This setting can be used to enable or disable a setting
+
+        Args:
+            key: key of the setting 
+            title: title of the setting 
+            description: description of the setting 
+            default: default value of the setting 
+            folder: if not None, near the setting it will be shown a button to open the specified folder 
+            website: if not None, near the setting it will be shown a button to open the specified website 
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed 
+            refresh_icon: if not None, the icon of the refresh button 
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = ExtraSettings.Setting(key, title, description, default, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "toggle"
         return r
@@ -46,6 +114,23 @@ class ExtraSettings:
     @staticmethod 
     def NestedSetting(key:str, title: str, description: str, extra_settings: list, 
                       folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new nested setting, which can be used to wrap other settings.
+
+        Args:
+            key: key of the setting  
+            title: title of the setting 
+            description: description of the setting 
+            extra_settings: list of extra settings 
+            folder: if not None, near the setting it will be shown a button to open the specified folder 
+            website: if not None, near the setting it will be shown a button to open the specified website 
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed 
+            refresh_icon: if not None, the icon of the refresh button 
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = ExtraSettings.Setting(key, title, description, None, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "nested"
         r["extra_settings"] = extra_settings
@@ -54,6 +139,28 @@ class ExtraSettings:
     @staticmethod 
     def DownloadSetting(key:str, title: str, description: str, is_installed: bool, callback: Callable, download_percentage: Callable, download_icon: str|None = None, 
                         folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new download setting. This will show a row with something that is downloadable. 
+        When clicked, it will start the download and show a progressbar.
+        If installed, a delete button will be shown.
+
+        Args:
+            key: key of the setting 
+            title: title of the setting               
+            description: description of the setting             
+            is_installed: if True, the delete button will be shown                         
+            callback: the function that will be executed when the download or delete button is clicked. Must download the file ON THE SAME THREAD                                            
+            download_percentage: the function that will be executed to get the download percentage (float between 0.0 and 1.0)                                                           
+            download_icon: if not None, the icon of the download button                                                                       
+            folder: if not None, near the setting it will be shown a button to open the specified folder
+            website: if not None, near the setting it will be shown a button to open the specified website
+            update_settings: if True, when the setting is changed, the settings will be automatically updated 
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed                                                                                       
+            refresh_icon: if not None, the icon of the refresh button                                                                                                                          
+
+        Returns:
+            
+        """
         r = ExtraSettings.Setting(key, title, description, is_installed, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "download"
         r["callback"] = callback
@@ -66,6 +173,26 @@ class ExtraSettings:
     @staticmethod
     def ButtonSetting(key:str, title: str, description: str, callback: Callable, label: str|None = None, icon: str|None = None,
                       folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new button setting. 
+        This setting shows a row with button and does not actually get a value.
+
+        Args:
+            key: key of the setting 
+            title: title of the setting 
+            description: description of the setting
+            callback: the function that will be executed when the button is clicked                            
+            label: if not None, the label of the button                                                               
+            icon: if not None, the icon of the button                                    
+            folder: if not None, near the setting it will be shown a button to open the specified folder
+            website: if not None, near the setting it will be shown a button to open the specified website
+            update_settings: if True, when the setting is changed, the settings will be automatically updated                                                                            
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed                                                                                       
+            refresh_icon: if not None, the icon of the refresh button                                                                                                                          
+
+        Returns:
+            dict: the setting in this format 
+        """
         r = ExtraSettings.Setting(key, title, description, None, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "button"
         r["callback"] = callback
@@ -78,6 +205,24 @@ class ExtraSettings:
     @staticmethod
     def ComboSetting(key: str, title: str, description: str, values: list | dict | tuple, default: str,
                      folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new combo setting. Shows a list of values to pick from
+
+        Args:
+            key: key of the setting                 
+            title: title of the setting               
+            description: description of the setting             
+            values: the values of the setting                              
+            default: the default value of the setting                       
+            folder: if not None, near the setting it will be shown a button to open the specified folder                                
+            website: if not None, near the setting it will be shown a button to open the specified website                                                                                  
+            update_settings: if True, when the setting is changed, the settings will be automatically updated                                                                            
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed                                                                                       
+            refresh_icon: if not None, the icon of the refresh button
+
+        Returns:
+            
+        """
         r = ExtraSettings.Setting(key, title, description, default, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "combo"
         values = ExtraSettings.fix_models_format(values)
@@ -106,6 +251,26 @@ class ExtraSettings:
     @staticmethod 
     def ScaleSetting(key: str, title: str, description: str, default: float, min: float, max: float, round: int,
                      folder: str|None = None, website: str|None = None, update_settings: bool = False, refresh: Callable|None = None, refresh_icon: str|None = None) -> dict:
+        """
+        Create a new scale setting. Used for numeric values, shows a slider.
+
+        Args:
+            key: key of the setting 
+            title: title of the setting 
+            description: description of the setting             
+            default: the default value of the setting                             
+            min: the minimum value of the setting                                   
+            max: the maximum value of the setting                               
+            round: the number of digits to round to                             
+            folder: if not None, near the setting it will be shown a button to open the specified folder                               
+            website: if not None, near the setting it will be shown a button to open the specified website                                                                                  
+            update_settings: if True, when the setting is changed, the settings will be automatically updated                                                                            
+            refresh: if not None, near the setting it will be shown a button to refresh the specified function. When clicked the function is executed                                                                                       
+            refresh_icon: if not None, the icon of the refresh button                                                                                                                          
+
+        Returns:
+             - dict: the setting in this format
+        """
         r = ExtraSettings.Setting(key, title, description, default, folder, website, update_settings, refresh, refresh_icon)
         r["type"] = "range"
         r["min"] = min
