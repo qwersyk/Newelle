0825c5fd2c43a74d215e69f790e9746c76b03e28
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-05-18 17:52:44 +0200

    Fix emoji removal

--- src/utility/strings.py
+++ src/utility/strings.py
@@ -308,5 +308,5 @@ def remove_emoji(text):
             u"\U0001F300-\U0001F5FF"  # symbols & pictographs
             u"\U0001F680-\U0001F6FF"  # transport & map symbols
             u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    )
+            "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
