bf5790166b10d2ac1f4a7c35633af93a92caa7cd
Oliver Tzeng <olivertzeng@proton.me>
2025-05-19 15:37:06 +0800

    fix(POTFILES): added more files in POTFILES after investigating, it seems that flatpak has .in files in the data directory that needs to be in POTFILES

--- build_locale.sh
+++ build_locale.sh
@@ -1,5 +1,6 @@
-fd -e py >po/POTFILES
-xgettext -o po/newelle.pot $(fd -e py)
+fd -t f -e py src | xargs rg -l "_\(|N\(_" >po/POTFILES
+fd -t f -e in data >>po/POTFILES
+xgettext -o po/newelle.pot $(cat po/POTFILES)
 cd po
 for file in $(fd -e po); do
 	msgmerge -U "$file" newelle.pot
