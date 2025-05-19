b25abd6c0eac0d76d61cf7dbcb329b24dd5ec53a
Oliver Tzeng <olivertzeng@proton.me>
2025-05-19 15:51:49 +0800

    fix(POTFILES): added xml files

--- build_locale.sh
+++ build_locale.sh
@@ -1,5 +1,5 @@
 fd -t f -e py src | xargs rg -l "_\(|N\(_" >po/POTFILES
-fd -t f -e in data >>po/POTFILES
+fd -t f '.*\.xml$|.*\.in$' data >>po/POTFILES
 xgettext -o po/newelle.pot $(cat po/POTFILES)
 cd po
 for file in $(fd -e po); do
