cc20988a2017ddfc4f1bf3d48ebba116db28cecf
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-06-19 09:16:22 +0200

    Add Prompt and Handler descriptors for extensions

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,8 +1,11 @@
 from .handler import Handler, ErrorSeverity
 from .extra_settings import ExtraSettings
+from .descriptors import HandlerDescription, PromptDescription
 
 __all__ = [
     "Handler",
     "ExtraSettings",
-    "ErrorSeverity"
+    "ErrorSeverity",
+    "HandlerDescription",
+    "PromptDescription"
 ]
