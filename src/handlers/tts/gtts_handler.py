04ab8816631141de273782fb8798747d2c790f8e
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-05-09 17:56:35 +0200

    Make gtts optional

--- src/handlers/tts/gtts_handler.py
+++ src/handlers/tts/gtts_handler.py
@@ -1,11 +1,14 @@
-from gtts import gTTS, lang
-
 from .tts import TTSHandler
 
 class gTTSHandler(TTSHandler):
     key = "gtts"
-   
+    
+    @staticmethod
+    def get_extra_requirements() -> list:
+        return ["gtts"]
+
     def get_voices(self):
+        from gtts import gTTS, lang
         if len(self.voices) > 0:
             return self.voices
         x = lang.tts_langs()
@@ -17,6 +20,7 @@ class gTTSHandler(TTSHandler):
         return res
 
     def save_audio(self, message, file):
+        from gtts import gTTS, lang
         voice = self.get_current_voice()
         if not self.voice_available(voice):
             voice = self.get_voices()[0][1]
